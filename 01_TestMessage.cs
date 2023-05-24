d063c8308c2973e71c2917d0edd7cd7577482492
Turtle Wolfe <JonPohlner@gmail.com>
2023-05-23 20:09:33 -0400

    Add project files.

---
+++ 01_TestMessage.cs
@@ -0,0 +1,35 @@
+﻿#region Namespaces
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+#endregion
+namespace Revit_Course
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    internal class _01_TestMessage:IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            //UIApplication uiapp = commandData.Application;
+            //UIDocument uidoc = uiapp.ActiveUIDocument;
+            //Application app = uiapp
+
+            //Document doc = uidoc.Document;
+
+            //Selection sel = uidoc.Selection;
+            
+            MessageBox.Show("Revit Course 01 Test Message");
+
+            //throw new NotImplementedException();
+            return Result.Succeeded;
+        }
+    }
+}
